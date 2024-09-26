7d62e64443d39c240a68e2f1cec38923e83a9778
Studio | Graficare <studio@Mac-Studio-van-Studio.local>
2024-09-26 16:13:15 +0200

    IN MY PROGRAMMER ERA
    :'D

---
+++ Lisa-Prototypes-Unity/Assets/Scripts/Score.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+    public static Score instance; 
+    public Text scoreText; 
+    public Text highscoreText; 
+
+    int score = 0; 
+    int highscore = 0; 
+
+    private void Awake()
+    {
+        instance = this; 
+    }
+
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0); 
+        scoreText.text = score.ToString() + "POINTS";
+        highscoreText.text = "HIGHSCORE: " + highscore.ToString(); 
+    }
+
+    public void AddPoint(int pointValue)
+    {
+        score = score + pointValue;
+        scoreText.text = score.ToString(); 
+
+        if (highscore < score)
+        PlayerPrefs.SetInt("highscore", score); 
+    }
+}
